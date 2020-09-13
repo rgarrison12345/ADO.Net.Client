8688705e3a95d9be555e1c64a794c8ca8c49210f
Robert Garrison <robert.garrison91@gmail.com>
2020-09-13 09:50:25 -0400

    intellisense update

--- src/ADO.Net.Client.Core/QueryFactory.cs
+++ src/ADO.Net.Client.Core/QueryFactory.cs
@@ -36,21 +36,21 @@ namespace ADO.Net.Client.Core
     {
         #region Methods
         /// <summary>
-        /// Create an instance of <see cref="ISqlQuery"/> using the <paramref name="parameters"/> and built sql query for a stored procedure
+        /// Create an instance of <see cref="ISqlQuery"/> using the <paramref name="parameters"/> and <paramref name="storedProcedureName"/> for a stored procedure
         /// </summary>
         /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> assocatiated with a query</param>
-        /// <param name="queryText">The Ad-Hoc query or stored procedure name</param>
+        /// <param name="storedProcedureName">Stored procedure name</param>
         /// <param name="shouldBePrepared">Indicates if the current sql string needs to be prepared (or compiled) version of the command on the data source.</param>
         /// <param name="commandTimeout">The wait time in seconds before terminating the attempt to execute a command and generating an error</param>
-        public static ISqlQuery CreateStoredProcedureQuery(string queryText, IEnumerable<DbParameter> parameters = null, int commandTimeout = 30, bool shouldBePrepared = false)
+        public static ISqlQuery CreateStoredProcedureQuery(string storedProcedureName, IEnumerable<DbParameter> parameters = null, int commandTimeout = 30, bool shouldBePrepared = false)
         {
-            return CreateSQLQuery(queryText, CommandType.StoredProcedure, parameters, commandTimeout, shouldBePrepared);
+            return CreateSQLQuery(storedProcedureName, CommandType.StoredProcedure, parameters, commandTimeout, shouldBePrepared);
         }
         /// <summary>
         /// Create an instance of <see cref="ISqlQuery"/> using the <paramref name="parameters"/> and built sql query
         /// </summary>
         /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> assocatiated with a query</param>
-        /// <param name="queryText">The Ad-Hoc query or stored procedure name</param>
+        /// <param name="queryText">The Ad-Hoc query</param>
         /// <param name="shouldBePrepared">Indicates if the current sql string needs to be prepared (or compiled) version of the command on the data source.</param>
         /// <param name="commandTimeout">The wait time in seconds before terminating the attempt to execute a command and generating an error</param>
         public static ISqlQuery CreateAdHocQuery(string queryText, IEnumerable<DbParameter> parameters = null, int commandTimeout = 30, bool shouldBePrepared = false)
