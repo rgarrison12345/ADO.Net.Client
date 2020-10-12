4c7ce34d40b825c715f3510429040bbc00abfe69
Robert Garrison <robert.garrison@amwins.com>
2020-10-12 12:26:11 -0400

    intellisense updates

--- src/ADO.Net.Client.Core/IMultiResultReaderAsync.cs
+++ src/ADO.Net.Client.Core/IMultiResultReaderAsync.cs
@@ -44,7 +44,7 @@ namespace ADO.Net.Client.Core
         /// Gets an <see cref="IAsyncEnumerable{T}"/> based on the <typeparamref name="T"/> streamed from the server asynchronously
         /// </summary>
         /// <typeparam name="T">An instance of the type the caller wants create from the query passed into procedure</typeparam>
-        /// <param name="token">Structure that propogates a notification that an operation should be cancelled</param>
+        /// <param name="token">Structure that propagates a notification that an operation should be cancelled</param>
         /// <returns>Returns an instance of <see cref="IAsyncEnumerable{T}"/></returns>
         IAsyncEnumerable<T> ReadObjectsStreamAsync<T>(CancellationToken token = default) where T : class;
 #endif
@@ -52,21 +52,21 @@ namespace ADO.Net.Client.Core
         /// Gets an entire <see cref="IEnumerable{T}"/> of <typeparamref name="T"/> asynchronously
         /// </summary>
         /// <typeparam name="T">An instance of the type the caller wants create from the query passed into procedure</typeparam>
-        /// <param name="token">Structure that propogates a notification that an operation should be cancelled</param>
+        /// <param name="token">Structure that propagates a notification that an operation should be cancelled</param>
         /// <returns>Returns an instance of <see cref="IEnumerable{T}"/> as an entire collection of <typeparamref name="T"/></returns>
         Task<IEnumerable<T>> ReadObjectsAsync<T>(CancellationToken token = default) where T : class;
         /// <summary>
         /// Gets a single instance of <typeparamref name="T"/> asynchronously
         /// </summary>
         /// <typeparam name="T">An instance of the type the caller wants create from the query passed into procedure</typeparam>
-        /// <param name="token">Structure that propogates a notification that an operation should be cancelled</param>
+        /// <param name="token">Structure that propagates a notification that an operation should be cancelled</param>
         /// <returns>Gets an instance of <typeparamref name="T"/></returns>
         Task<T> ReadObjectAsync<T>(CancellationToken token = default) where T : class;
         /// <summary>
         /// Moves to next result set in the underlying data set asynchronously
         /// </summary>
-        /// <param name="token">Structure that propogates a notification that an operation should be cancelled</param>
-        /// <returns>Returns <c>true</c> if there's another result set in the dataset <c>false</c> otherwise</returns>
+        /// <param name="token">Structure that propagates a notification that an operation should be cancelled</param>
+        /// <returns>Returns <c>true</c> if there's another result set in the data set <c>false</c> otherwise</returns>
         Task<bool> MoveToNextResultAsync(CancellationToken token = default);
 #if ADVANCED_FEATURES
         /// <summary>
