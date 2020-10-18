11956f034ccc8fa2454a68317210148eb91102ab
Robert Garrison <robert.garrison@amwins.com>
2020-10-18 09:00:22 -0400

    intellisense update

--- src/ADO.Net.Client.Core/QueryFactory.cs
+++ src/ADO.Net.Client.Core/QueryFactory.cs
@@ -38,7 +38,7 @@ namespace ADO.Net.Client.Core
         /// <summary>
         /// Create an instance of <see cref="ISqlQuery"/> using the <paramref name="parameters"/> and <paramref name="storedProcedureName"/> for a stored procedure
         /// </summary>
-        /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> assocatiated with a query</param>
+        /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> associated with a query</param>
         /// <param name="storedProcedureName">Stored procedure name</param>
         /// <param name="shouldBePrepared">Indicates if the current sql string needs to be prepared (or compiled) version of the command on the data source.</param>
         /// <param name="commandTimeout">The wait time in seconds before terminating the attempt to execute a command and generating an error</param>
@@ -49,7 +49,7 @@ namespace ADO.Net.Client.Core
         /// <summary>
         /// Create an instance of <see cref="ISqlQuery"/> using the <paramref name="parameters"/> and built sql query
         /// </summary>
-        /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> assocatiated with a query</param>
+        /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> associated with a query</param>
         /// <param name="queryText">The Ad-Hoc query</param>
         /// <param name="shouldBePrepared">Indicates if the current sql string needs to be prepared (or compiled) version of the command on the data source.</param>
         /// <param name="commandTimeout">The wait time in seconds before terminating the attempt to execute a command and generating an error</param>
@@ -60,7 +60,7 @@ namespace ADO.Net.Client.Core
         /// <summary>
         /// Create an instance of <see cref="ISqlQuery"/> using the <paramref name="parameters"/> and built sql query
         /// </summary>
-        /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> assocatiated with a query</param>
+        /// <param name="parameters">An <see cref="IEnumerable{T}"/> of <see cref="DbParameter"/> associated with a query</param>
         /// <param name="queryText">The Ad-Hoc query or stored procedure name</param>
         /// <param name="shouldBePrepared">Indicates if the current sql string needs to be prepared (or compiled) version of the command on the data source.</param>
         /// <param name="commandTimeout">The wait time in seconds before terminating the attempt to execute a command and generating an error</param>
