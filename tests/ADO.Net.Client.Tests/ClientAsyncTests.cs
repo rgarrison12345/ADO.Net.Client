fb63cdfb62e96d2ce3d6815bbe94a50419116f0e
Robert Garrison <robert.garrison@amwins.com>
2020-10-24 09:42:23 -0400

    cleanup

--- tests/ADO.Net.Client.Tests/ClientAsyncTests.cs
+++ tests/ADO.Net.Client.Tests/ClientAsyncTests.cs
@@ -22,7 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 #endregion
 #region Using Statements
-
 using System;
 using ADO.Net.Client.Core;
 using ADO.Net.Client.Implementation;
